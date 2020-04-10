d960572fd54cfc6dfc25a7a779f395f25d340079
João Victor <jvcd1999@hotmail.com>
2020-04-10 16:21:57 -0300

    Implementando botão de Jogar e Sair

---
+++ TCC/Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play() {
+        StartCoroutine(ChangeScene());
+    }
+
+    public void Quit() {
+        StartCoroutine(CloseScene());
+    }
+
+    public IEnumerator ChangeScene()
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public IEnumerator CloseScene()
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+  
+}
