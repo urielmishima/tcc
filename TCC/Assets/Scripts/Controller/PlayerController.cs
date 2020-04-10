f3149e8ddd6c101274d23b663a8b6a8de63f0a38
Uriel Mishima <uriel.hmfm@gmail.com>
2020-04-10 15:01:35 -0300

    Criando classe de interativos

---
+++ TCC/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Camera cam;
+    public FlatInteractable focus;
+    
+    [SerializeField]
+    private float interactRange = 1f;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, interactRange))
+            {
+                FlatInteractable interactable = hit.collider.GetComponent<FlatInteractable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+            }
+        }
+        
+    }
+
+    void SetFocus(FlatInteractable newFocus)
+    {
+
+        if (newFocus != focus)
+        {
+            if(focus != null)
+                focus.OnDefocused();
+
+            focus = newFocus;
+        }
+
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        focus = null;
+    }
+
+}
---
+++ TCC/Assets/Scripts/ItemPickUp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ItemPickUp : FlatInteractable
+{
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+
+    void PickUp()
+    {
+        Debug.Log("Picking up item.");
+    }
+}
