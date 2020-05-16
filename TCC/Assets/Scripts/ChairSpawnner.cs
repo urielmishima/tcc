f65c7627c4065a0e7e664696184968d0c0c8a041
Uriel Mishima <uriel.hmfm@gmail.com>
2020-05-16 18:44:53 -0300

    action-chair-shoted
    Adicionando ação de jogar a carteira após a interação com o papel no banheiro

---
+++ TCC/Assets/Scripts/ChairSpawnner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ChairSpawnner : MonoBehaviour
+{
+    [SerializeField] private GameObject chairPrefab;
+    
+    private bool hasTriggered;
+
+    public UnityEvent chairInvoked;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Colidiu");
+        if (!hasTriggered)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                chairInvoked?.Invoke();
+                StartCoroutine(invokeChair());                
+            }
+        }
+    }
+
+    private IEnumerator invokeChair()
+    {
+        yield return new WaitForSeconds(1f);
+        GameObject chair = Instantiate(chairPrefab, transform.position, new Quaternion(0f, 0f, 0f, 0f));
+        Rigidbody chairRigidbody = chair.GetComponent<Rigidbody>();
+        chairRigidbody.AddForce(transform.forward * 40, ForceMode.Impulse);
+        chairRigidbody.AddTorque(transform.up * 50, ForceMode.Impulse);
+        chairRigidbody.AddTorque(transform.right * 50, ForceMode.Impulse);
+        hasTriggered = true;
+    }
+}
