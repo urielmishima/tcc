3d5eb3fbf42433fff93a0a41dd67b134648b8171
Uriel Mishima <uriel.hmfm@gmail.com>
2020-09-12 00:25:20 -0300

    movimentação do nobody

---
+++ TCC/Assets/Scripts/NavigationAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class NavigationAnimation : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Forward", agent.velocity.magnitude);
+    }
+}
