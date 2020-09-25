4c5937c29f63e49cb8a5c719dbb921d94f793c1f
Uriel Mishima <uriel.hmfm@gmail.com>
2020-09-25 20:15:25 -0300

    Adicionando script do menu

---
+++ TCC/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    private static bool GameIsPaused;
+    [SerializeField] private GameObject pauseMenuUI;
+
+    public UnityEvent onResumeByButton;
+
+    private void Start()
+    {
+        GameIsPaused = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused) Resume(false);
+            else Pause();
+        }
+    }
+
+    public void Resume(bool button)
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        if (button) onResumeByButton?.Invoke();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+
+}
