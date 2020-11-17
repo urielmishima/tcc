b8cb5c591c779647ca1aad8910d9e516cd59a268
Uriel Mishima <uriel.hmfm@gmail.com>
2020-11-17 00:42:39 -0300

    get-out-church-incomplete

---
+++ TCC/Assets/Scripts/BackSceneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackSceneController : IInteractable
+{
+    void Start()
+    {
+        GUIText = "interact";
+    }
+
+    public override void OnStartLook()
+    {
+        ShowText = true;
+    }
+    public override void OnInteract()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public override void OnEndLook()
+    {
+        ShowText = false;
+    }
+}
